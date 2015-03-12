a15fbee31b5cdbfb2cb36e4e08ecda8e3ead0731
Yoan Mollard <yoan.mollard@inria.fr>
2015-03-12 23:20:43 +0100

    Added Arbapp and an example of application

---
+++ src/Arbapp.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+"""
+    Arbalet - ARduino-BAsed LEd Table
+    Arbapp - Arbalet Application
+
+    All Application for Arbalet should inherit from this class.
+    Wanna create an awesome Arbalet application? Start here.
+
+    Copyright (C) 2015 Yoan Mollard <yoan@konqifr.fr>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+"""
+
+from Arbamodel import *
+from Arbalet import *
+import sys
+class Arbapp(object):
+
+    # Authorized arguments
+    authorized_opts = ["simulation", "hardware", "s", "a"]
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        opts = self.readopts(sys.argv)
+        self.arbalet = Arbalet("simulation" in opts or "s" in opts or len(opts)==0,
+                               "hardware" in opts or "a" in opts,
+                               width, height)
+        self.run()
+        self.close("Program naturally ended")
+
+    def readopts(self, argv):
+        return [x.lstrip('-') for x in argv if x.lstrip('-') in self.authorized_opts]
+
+    def set_model(self, model):
+        self.arbalet.set_model(model)
+
+    def run(self):
+        raise Exception("Arbapp.run() must be overidden")
+
+    def close(self, reason='unknown'):
+        self.arbalet.close(reason)
---
+++ src/Example.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+"""
+    Arbalet - ARduino-BAsed LEd Table
+    Example - Arbalet Program Example
+
+    Copyright (C) 2015 Yoan Mollard <yoan@konqifr.fr>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+"""
+
+from Arbapp import *
+from Arbamodel import *
+
+class Example(Arbapp):
+    def run(self):
+
+        time.sleep(1)
+
+        # We construct 2 different models to give an example
+        model_red = Arbamodel(self.width, self.height)
+        model_green = Arbamodel(self.width, self.height)
+
+        # The "green" model is filled in background but not shown
+        for w in range(self.width):
+            for h in range(self.height):
+                model_green.set_pixel(h, w, 'green')
+
+        # The "red" model is now shown
+        self.set_model(model_red)
+
+        # The "red" model is updated by reference, shown in live
+        for w in range(self.width):
+            for h in range(self.height):
+                model_red.set_pixel(h, w, 'red')
+                time.sleep(0.05)
+
+        time.sleep(1)
+
+        # We switch immediately to the "green" model...
+        self.set_model(model_green)
+        time.sleep(1)
+
+        # ...and back to the "red" again
+        self.set_model(model_red)
+        time.sleep(1)
+
+# Actual call starting the program
+Example(width = 10, height = 15)
