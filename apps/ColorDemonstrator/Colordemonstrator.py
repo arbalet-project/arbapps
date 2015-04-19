ca0d0edd30988dcc02673b973397b9d5370cdd04
Yoan Mollard <yoan.mollard@inria.fr>
2015-04-20 01:25:17 +0200

    CD, major switch to python generators

--- apps/ColorDemonstrator/Colordemonstrator.py
+++ apps/ColorDemonstrator/Colordemonstrator.py
@@ -22,58 +22,82 @@
 import time
 import random
 from arbasdk import Arbapp, Arbapixel
-from threading import Thread
 
-class LivingPixel(Thread):
-    def __init__(self, h, w, model, colors, max_rate, duration):
-        Thread.__init__(self)
-        self.h = h
-        self.w = w
-        self.model = model
-        self.max_rate = max_rate
-        self.duration = duration
-        self.colors = colors
-        self.running = True
+def rescale(val, in_min, in_max, out_min, out_max):
+    return out_min + (val - in_min) * ((out_max - out_min) / float(in_max - in_min))
 
-    def fade_colors(self, h, w, duration):
-        num_steps = int(self.max_rate*duration)
-        pairs = zip(self.colors, self.colors[1:])
+def LifeGenerator0(colors, duration):
+    """
+    Generator of pixel values browsing the given colors
+    :param colors: The list of colors to browse
+    :param duration: Duration of a single color change in number of frames (int from 1 to n)
+    :return: the yielded next value of this pixel
+    """
+    pairs = zip(colors, colors[1:])
+    while True:
         for color1, color2 in pairs:
-            for v in range(num_steps):
-                col = Arbapixel(color1)*((num_steps-v)/float(num_steps)) + Arbapixel(color2)*(v/float(num_steps))
-                self.model.set_pixel(h, w, col)
-                time.sleep(duration/float(num_steps))
-                if not self.running: return
+            for v in range(duration):
+                col = Arbapixel(color1)*((duration-v)/float(duration)) + Arbapixel(color2)*(v/float(duration))
+                yield col
 
-    def stop(self):
-        self.running = False
+def swipe_async(n_frames, n_colors):
+    pairs = zip(range(n_colors), range(1, n_colors))
+    n_swipe = n_frames/n_colors # TODO %n_colors=0
+    color_generator = [[]]
+    raise NotImplementedError()
 
-    def run(self):
-        while self.running:
-            self.fade_colors(self.h, self.w, self.duration)
+def gen_random_flashing(n_frames, n_colors):
+    if n_frames%2==1:  # We need an odd number of frames since we are going to divide them by two
+        n_frames+=1
+    base_exp = [1.1**(p-n_frames/2+1) for p in range(n_frames/2)]  # The first n_frames/2 are an exponential rise
+    reverse_base = [base_exp[i] for i in range(n_frames/2-1, -1, -1)]
+    white_generator = base_exp+reverse_base
+    blue_generator = [1.0-i for i in white_generator]
+    color_generator = [blue_generator, white_generator]
+
+    return color_generator
 
 class ColorDemo(Arbapp):
-    def __init__(self, width, height, colors, dur_min, dur_max, max_rate = 10):
+    generators = [gen_random_flashing, ]
+
+    def __init__(self, width, height, colors, rate, dur_min, dur_max, generator_id):
         Arbapp.__init__(self, width, height)
-        self.max_rate = max_rate  # Max refreshing rate in Hz
-        self.durations = [dur_min, dur_max]
-        self.threads = []
+        self.durations = [int(dur_min*rate), int(dur_max*rate)]
+        self.rate = rate
         if colors[:-1]!=colors[0]:
             colors.append(colors[0])
         self.colors = colors
+        self.generator = self.generators[generator_id]
+
+    def apply_color_to_generator(self, colors, color_generator):
+        while True:
+            for s in range(len(color_generator[0])):
+                col = Arbapixel(colors[0])*color_generator[0][s] + Arbapixel(colors[1])*color_generator[1][s]
+                yield col
 
     def run(self):
-        for w in range(self.width):
-            for h in range(self.height):
-                duration = random.randrange(self.durations[0]*10, self.durations[1]*10)/10.
-                self.threads.append(LivingPixel(h, w, self.model, self.colors, self.max_rate, duration))
-        for t in self.threads:
-            t.start()
-        raw_input('Press enter to kill Color Demonstrator')
-        for t in self.threads:
-            t.running = False
+        # Construct all pixel generators
+        generators = [[None for w in range(self.width)] for h in range(self.height)]
+        for h in range(self.height):
+            for w in range(self.width):
+                duration = random.randrange(self.durations[0], self.durations[1])
+                generators[h][w] = self.apply_color_to_generator(self.colors, self.generator(duration, len(self.colors)))
 
-e = ColorDemo(width = 10, height = 15, colors=['navy', 'gold', 'deeppink', 'yellowgreen', 'purple'],
-              dur_min=10, dur_max=60)
+        # Browse all pixel generators at each time
+        while True:
+            for h in range(self.height):
+                for w in range(self.width):
+                    try:
+                        color = generators[h][w].next()
+                    except StopIteration:
+                        pass
+                    else:
+                        self.model.set_pixel(h, w, color)
+            time.sleep(1./self.rate)
+
+
+#e = ColorDemo(width = 10, height = 15, colors=['navy', 'gold', 'deeppink', 'yellowgreen', 'purple'], rate=20, dur_min=10, dur_max=30)
+e = ColorDemo(width = 10, height = 15, colors=['darkblue', 'white'], rate=20, dur_min=10, dur_max=60, generator_id=0)
+
 e.run()
 e.close("end")
