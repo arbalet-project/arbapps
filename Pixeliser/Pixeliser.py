a105e290071121c3a85c9bcde830367b3cd4f729
Yoan Mollard <yoan.mollard@inria.fr>
2015-06-07 03:00:17 +0200

    Pixeliser: the opncv image must be an option + fps = nan?

--- Pixeliser/Pixeliser.py
+++ Pixeliser/Pixeliser.py
@@ -21,6 +21,8 @@ class Pixeliser(Arbapp):
     def play_file(self, f):
         if isfile(f):
             self.video_reader = cv2.VideoCapture(f)
+            self.rate = self.video_reader.get(cv2.cv.CV_CAP_PROP_FPS)
+            print self.rate, 'fps'
 
             while True:
                 r, image = self.video_reader.read()
@@ -28,8 +30,9 @@ class Pixeliser(Arbapp):
                     pix_image = cv2.resize(image, (self.height, self.width))
                     pix_image = cv2.transpose(pix_image)
                     self.update_model(pix_image)
-                    cv2.imshow(f, image)
-                    cv2.waitKey(100)
+                    if self.args.display_original:
+                        cv2.imshow(f, image)
+                    cv2.waitKey(80)
                 else:
                     break
         else:
@@ -56,5 +59,11 @@ if __name__=='__main__':
                         required=True,
                         nargs='+',
                         help='Video file(s) to pixelise')
-    
+
+    parser.add_argument('-do', '--display-original',
+                        action='store_const',
+                        const=True,
+                        default=False,
+                        help='Display the original video in an OpenCV window (require access to X display)')
+
     Pixeliser(parser).start()
