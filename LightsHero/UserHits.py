b4a23e6b97b309a3e43151128cedae60011f24cc
Yoan Mollard <yoan.mollard@inria.fr>
2015-06-09 00:38:40 +0200

    LH: use pygame events for keyboard reading

--- LightsHero/UserHits.py
+++ LightsHero/UserHits.py
@@ -1,4 +1,4 @@
-from pygame import key, K_F1, K_F2, K_F3, K_F4, K_F5, error
+from pygame import K_F1, K_F2, K_F3, K_F4, K_F5, KEYDOWN, KEYUP, event
 
 class UserHits():
     """
@@ -13,6 +13,7 @@ class UserHits():
         self.max_score = 0  # measures the maximum score that the user could have
         self.num_lanes = num_lanes
         self.active_notes = [False]*num_lanes
+        self.keys = [False]*num_lanes
 
     def set_note(self, num_lane, active):
         self.active_notes[num_lane] = active
@@ -23,21 +24,28 @@ class UserHits():
         :return: vector of one boolean per lane True if the corresponding lane is being pressed
         """
         # 1. Get pressed keys
-        try:
-            keys = [key.get_pressed()[K_F1], key.get_pressed()[K_F2], key.get_pressed()[K_F3], key.get_pressed()[K_F4], key.get_pressed()[K_F5]]
-        except error:
-            # An error occurs if pygame is stopped
-            keys = [False]*self.num_lanes
+        for evt in event.get():
+            if evt.type in [KEYDOWN, KEYUP]:
+                if evt.key==K_F1:
+                    self.keys[0] = evt.type==KEYDOWN
+                elif evt.key==K_F2:
+                    self.keys[1] = evt.type==KEYDOWN
+                elif evt.key==K_F3:
+                    self.keys[2] = evt.type==KEYDOWN
+                elif evt.key==K_F4:
+                    self.keys[3] = evt.type==KEYDOWN
+                elif evt.key==K_F5:
+                    self.keys[4] = evt.type==KEYDOWN
 
         # 2. Update the score and the maximum score
         for lane in range(self.num_lanes):
-            if keys[lane] and self.active_notes[lane]:
+            if self.keys[lane] and self.active_notes[lane]:
                 self.score += 10
-            elif keys[lane] and not self.active_notes[lane]:
+            elif self.keys[lane] and not self.active_notes[lane]:
                 self.score -= 2
 
             if self.active_notes[lane]:
                 self.max_score += 10
-        return keys
+        return self.keys
 
 
