630d7b0dec03b4cbc6eeb7572745799d7d2dfbb7
Yoan Mollard <yoan.mollard@inria.fr>
2015-06-16 20:09:33 +0200

    Proper timing of GIFs and rotation

--- ImageReader/ImageReader.py
+++ ImageReader/ImageReader.py
@@ -19,10 +19,13 @@ class ImageReader(Arbapp):
         Arbapp.__init__(self, argparser)
         self.image = None
         self.palette = None
+        self.vertical = False
 
-    def play_file(self, f, time):
+    def play_file(self, f):
         if isfile(f):
             self.image = Image.open(f)
+            if self.image.size[0]<self.image.size[1]:
+                self.vertical = True
             while True:
                 try:
                     self.update_model(self.image.convert('RGB'))
@@ -30,12 +33,9 @@ class ImageReader(Arbapp):
                 except EOFError:
                     if self.args.loop:
                         self.image.seek(0)
-                        sleep(1)
                     else:
-                        sleep(time)
-                else:
-                    print "New frame"
-                    sleep(0.1)
+                        return
+                sleep(self.image.info['duration']/1000.)  # Gif duration are in msec
         else:
             raise IOError('No such file or directory: \'{}\''.format(f))
 
@@ -43,13 +43,13 @@ class ImageReader(Arbapp):
         self.model.lock()
         for h in range(self.height):
             for w in range(self.width):
-                pixel = image.getpixel((h, w))
+                pixel = image.getpixel((w, h) if self.vertical else (h, w))
                 self.model.set_pixel(h, w, pixel)
         self.model.unlock()
 
     def run(self):
         for f in self.args.input:
-            self.play_file(f, 5)
+            self.play_file(f)
 
 
 if __name__=='__main__':
