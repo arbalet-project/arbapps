8fb86ff7d7009f2214f6d6cc61710e76f78341c2
Yoan Mollard <yoan.mollard@inria.fr>
2015-07-10 00:12:47 +0200

    Bounces: Unstable: Added leap motion integration

--- Bounces/Bounces.py
+++ Bounces/Bounces.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
     Arbalet - ARduino-BAsed LEd Table
     Bounces - Bouncing pixels propeled by motions over a Leap controller
@@ -9,11 +10,48 @@
 """
 from random import randint, uniform, choice
 from arbasdk import Arbapp, Arbapixel, Rate
+from threading import Lock
+from math import cos, sin
 import argparse
+
+
+
+import sys
+lib = '/home/yoan/Téléchargements/LeapDeveloperKit_2.2.6+29154_linux/LeapSDK/lib'
+sys.path.append(lib)
+
+import os, sys, inspect
+src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+arch_dir = lib+'/x64' if sys.maxsize > 2**32 else lib+'/x86'
+sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
+
+import Leap
+from Leap import SwipeGesture
+
+class SampleListener(Leap.Listener):
+    def __init__(self, swipe, swipe_lock):
+        Leap.Listener.__init__(self)
+        self.swipe = swipe
+        self.swipe_lock = swipe_lock
+        print "Leap listener is started!"
+
+    def on_connect(self, controller):
+        controller.enable_gesture(Leap.Gesture.TYPE_SWIPE)
+
+    def on_frame(self, controller):
+        frame = controller.frame()
+        for gesture in frame.gestures():
+            if gesture.type == Leap.Gesture.TYPE_SWIPE:
+                swipe = SwipeGesture(gesture)
+                with self.swipe_lock:
+                    self.swipe[0] = SwipeGesture(gesture)
+                print "  Swipe id: %d, state: %s, position: %s, direction: %s, speed: %f" % (
+                    swipe.id, swipe.state, swipe.position, swipe.direction, swipe.speed)
 
 class Ball():
     colors = ['deeppink', 'navy', 'gold', 'white', 'grey', 'darkgreen', 'chocolate']
-    def __init__(self, id, x, y, height, width, color='white', x_speed=0, y_speed=0):
+
+    def __init__(self, id, x, y, height, width, color='white', x_speed=0, y_speed=0, friction_factor=0.99, min_speed=0.1):
         """
 
         :param x:
@@ -29,6 +67,8 @@ class Ball():
         self.width, self.height = width, height
         self.color = color
         self.x_speed, self.y_speed = (x_speed, y_speed)
+        self.friction_factor = friction_factor
+        self.min_speed = min_speed
 
     @property
     def x(self):
@@ -56,6 +96,11 @@ class Ball():
             self.y_speed = -self.y_speed
             self.step_forward()
 
+        # Simulate friction, slow down
+        if abs(self.x_speed)>self.min_speed:
+            self.x_speed *= self.friction_factor
+        if abs(self.y_speed)>self.min_speed:
+            self.y_speed *= self.friction_factor
 
 class Bounces(Arbapp):
 
@@ -65,21 +110,38 @@ class Bounces(Arbapp):
         self.rate = Rate(rate)
 
         def rand():
-            return choice([-1, 1])*uniform(0.1, 1)
+            return choice([-1, 1])*uniform(1./rate, 10./rate)
 
-        for ball in range(11):
+        for ball in range(4):
             self.balls.append(Ball(ball, randint(0, self.height-1), randint(0, self.width-1),
                                    self.height, self.width,
                                    Ball.colors[ball%len(Ball.colors)],
                                    rand(), rand()))
 
+        # Motion control via Leap Motion
+        self.swipe = [None]
+        self.swipe_lock = Lock()
+        self.leap_listener = SampleListener(self.swipe, self.swipe_lock)
+        self.controller = Leap.Controller()
+        self.controller.add_listener(self.leap_listener)
 
+    def close(self, reason='unknown'):
+        Arbapp.close(self, reason)
+        self.controller.remove_listener(self.leap_listener)
 
     def render(self):
         self.model.lock()
         self.model.set_all('black')
-        for ball in self.balls:
-            self.model.set_pixel(ball.x, ball.y, ball.color)
+        with self.swipe_lock:
+            for ball in self.balls:
+                self.model.set_pixel(ball.x, ball.y, ball.color)
+                if self.swipe[0] is not None:
+                    # mapping axes (height, width) of Arbalet on axes (x, z) of Leap Motion
+                    x_speed_boost = self.swipe[0].direction[0] * self.swipe[0].speed / 1000.
+                    y_speed_boost = self.swipe[0].direction[2] * self.swipe[0].speed / 1000.
+                    ball.x_speed += x_speed_boost
+                    ball.y_speed += y_speed_boost
+            self.swipe[0] = None
         self.model.unlock()
 
     def run(self):
@@ -94,6 +156,8 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Bouncing pixels propelled by a leap motion controller'
                                                  'You must plug a Leap Motion controller, install its SDK and run its daemon first')
 
-    e = Bounces(parser, 10)
-    e.start()
-    e.close("end")
+    e = Bounces(parser, 50)
+    try:
+        e.start()
+    finally:
+        e.close("end")
