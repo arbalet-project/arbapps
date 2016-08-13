eb7b25fe40d62bf1554c650c59709b4f94897358
Yoan Mollard <yoan.mollard@inria.fr>
2016-08-13 03:23:00 +0200

    Colors: module + py3

--- arbalet/apps/colors/colors.py
+++ arbalet/apps/colors/colors.py
@@ -7,8 +7,8 @@
     License: GPL version 3 http://www.gnu.org/licenses/gpl.html
 """
 import random
-from arbasdk import Arbapp, Arbapixel, Rate
-import argparse
+from arbalet.core import Arbapp, Arbapixel, Rate
+
 
 def gen_sweep_async(n_frames, n_frames_fade, n_frames_rand, colors):
     # TODO randomization to change to avoid overlapping of farthest colors
@@ -123,7 +123,7 @@ class ColorDemo(Arbapp):
             for h in range(self.height):
                 for w in range(self.width):
                     try:
-                        color = generators[h][w].next()
+                        color = generators[h][w].__next__()
                     except StopIteration:
                         pass
                     else:
@@ -131,47 +131,3 @@ class ColorDemo(Arbapp):
             self.model.unlock()
             self.rate.sleep()
 
-if __name__=='__main__':
-    # Below is declared the dictionary of available effects
-    # Effects are generated thanks to Python generators: https://wiki.python.org/moin/Generators
-    # At a certain framerate the chosen generator will provide a new color, whether it has changed or not
-    # A generator is finite, it has a life, that can be different for each pixel
-    #
-    # - rate is the frame rate in Hz
-    # - dur_min is the minimum duration life in seconds (duration of the faster pixel)
-    # - dur_max is tha maximum duration life in seconds (duration of the slower pixel)
-    # the duration of a pixel life is picked randomly between dur_min and dur_max
-    # the small dur_max - dur_min is, the more synchronized all the pixels are
-    # - colors is the vector of colors to swipe in the given duration
-    
-    animations = {'swipe': { 'rate': 20, 'dur_min': 10, 'dur_max': 15, 'generator_id': 1,
-                             'colors': ['yellowgreen', 'darkorange', 'darkred', 'deeppink',
-                                        'purple', 'darkblue', 'turquoise', 'darkgreen'] },
-
-                  'african': { 'rate': 20, 'dur_min': 10, 'dur_max':15, 'generator_id': 2,
-                               'colors':[(39, 26, 19), (49, 32, 23), (100, 66, 48), (172, 69, 11), (232, 139, 36)] },
-
-                  'flashes': { 'rate': 20, 'dur_min': 10, 'dur_max': 60, 'generator_id': 0,
-                               'colors':['darkblue', 'white'] },
-
-                  'gender': { 'rate': 20, 'dur_min': 5, 'dur_max': 15, 'generator_id': 2,
-                               'colors':['darkblue', 'deeppink'] },
-
-                  'teddy':  { 'rate': 20, 'dur_min': 5, 'dur_max': 20, 'generator_id': 2,
-                               'colors':[(0, 30, 30), (20, 10, 0)] },
-
-                  'warm':  { 'rate': 50, 'dur_min': 10, 'dur_max': 30, 'generator_id': 2,
-                               'colors':[[20, 11, 2], [13, 0, 3], [10, 1, 4]] },
-
-                  }
-
-    parser = argparse.ArgumentParser(description='Color demonstrator with nice effects and animations for demos (and pleasure!)'
-                                                 'To be enriched with newer animations!')
-    parser.add_argument('-t', '--type',
-                        default='swipe',
-                        choices=animations.keys(),
-                        help='Type of effect')
-
-    e = ColorDemo(parser, animations)
-    e.start()
-    e.close("end")
